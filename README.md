facdb744eef597432894707515b83c71054593b0
june29 <june29.jp@gmail.com>
2019-05-21 00:50:05 +0900

    :baby: First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Takanawa
+
+Takanawa は Scrapbox の Slack 向け通知を受け取って、条件にマッチしたチャンネルに投稿を行うゲートウェイです。
+
+![Takanawa](takanawa.png)
+
+## Setup
+
+事前に https://github.com/google/clasp の導入は済ませておいてください。
+
+```
+$ npm install
+$ clasp create --title "Takanawa" --type sheets --rootDir ./src
+$ git checkout src/appsscript.json
+$ clasp push
+? Manifest file has been updated. Do you want to push and overwrite? Yes
+└─ src/Takanawa.ts
+└─ src/appsscript.json
+Pushed 2 files.
+$ clasp open
+```
+
+`clasp open` で Google Apps Script のエディタ画面が開くので、File メニューから Project properties を開き、Script properties に下記 4 つの環境変数を設定します。
+
+- `SCRAPBOX_HOST`
+  - Scrapbox のホストです。エンタープライズ版の場合はそのホスト名を、クラウド版の場合は `scrapbox.io` を設定します。
+- `SCRAPBOX_PROJECT`
+  - Scrapbox のプロジェクト名です。プロジェクトページの URL が https://scrapbox.io/june29 なら `june29` を設定します。
+- `SCRAPBOX_COOKIE`
+  - Scrapbox のプロジェクトがプライベート設定の場合、認証用に必要になります。
+- `SLACK_WEBHOOK_URL`
+  - Slack の Incoming Webhook の URL を設定します。
+
+Google Apps Script をウェブアプリケーションとして公開したら、その URL を Scrapbox の Slack 通知先に指定してください。
+
+## Rules
+
+シートにルールを記述します。1 行目はヘッダ行になりますので、説明でも書いておきましょう。
+
+![Rules](takanawa-rules.png)
+
+上記のスクリーンショットのようにルールを記述した場合、通知先チャンネルは以下のように選ばれます。
+
+- ページタイトルに aaa を含む場合、チャンネル #aaa に通知される
+- ページ本文に bbb を含む場合、チャンネル #bbb に通知される
+- ページのタイトルもしくは本文に ccc を含む、チャンネル #ccc に通知される
+- ページのタイトルに aaa を含み、かつ本文に bbb を含む場合、チャンネル #aaa と #bbb に通知される
+- どのルールにもマッチしない場合、Slack の Incoming Webhook で設定したチャンネルに通知される
---
+++ src/Takanawa.ts
@@ -0,0 +1,91 @@
+const property = PropertiesService.getScriptProperties();
+const SCRAPBOX_HOST = property.getProperty("SCRAPBOX_HOST");
+const SCRAPBOX_PROJECT = property.getProperty("SCRAPBOX_PROJECT");
+const SCRAPBOX_COOKIE = property.getProperty("SCRAPBOX_COOKIE");
+const SLACK_WEBHOOK_URL = property.getProperty("SLACK_WEBHOOK_URL");
+
+function doPost(event) {
+  const postData = JSON.parse(event.postData.getDataAsString());
+
+  for (var i = 0; i < postData.attachments.length; i++) {
+    const attachment = postData.attachments[i];
+    const title = attachment.title;
+    const body = fetchPageText(title);
+
+    const channels = matchRules(title, body);
+
+    if (channels.length > 0) {
+      for (var j = 0; j < channels.length; j++) {
+        postToSlack({ attachment: attachment, channel: channels[j] });
+      }
+    }
+    else {
+      postToSlack({ attachment: attachment });
+    }
+  }
+}
+
+function postToSlack(options) {
+  const payload = {
+    attachments: [options.attachment]
+  };
+
+  if (options.channel) {
+    payload["channel"] = options.channel;
+  }
+
+  const response = UrlFetchApp.fetch(SLACK_WEBHOOK_URL, { method: 'post', payload: JSON.stringify(payload) });
+  console.log({
+    response: response,
+    payload: payload
+  });
+}
+
+function fetchPageText(title) : string {
+  const url = `https://${SCRAPBOX_HOST}/api/pages/${SCRAPBOX_PROJECT}/${title}/text`;
+  const headers = { 'Cookie' : 'connect.sid=' + SCRAPBOX_COOKIE };
+  const response = UrlFetchApp.fetch(url, { method: "get", headers : headers });
+
+  return response.getContentText();
+}
+
+function matchRules(title : string, body : string) : Array<string> {
+  const channels = [];
+  const rules = loadRules();
+
+  for (var i = 0; i < rules.length; i++) {
+    const rule = rules[i];
+
+    if (rule.title.length > 0 && title.indexOf(rule.title) != -1) {
+      if (channels.indexOf(rule.channel) == -1) {
+        channels.push(rule.channel);
+      }
+    }
+
+    if (rule.body.length > 0 && body.indexOf(rule.body) != -1) {
+      if (channels.indexOf(rule.channel) == -1) {
+        channels.push(rule.channel);
+      }
+    }
+  }
+
+  return channels;
+}
+
+function loadRules() {
+  const rules = [];
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+
+  for (var i = 2; i <= sheet.getLastRow(); i++) {
+    const row = sheet.getRange(i, 1, 1, 3);
+    const values = row.getValues()[0];
+
+    const title = values[0];
+    const body = values[1];
+    const channel = values[2];
+
+    rules.push({ title: title, body: body, channel: channel });
+  }
+
+  return rules;
+}
